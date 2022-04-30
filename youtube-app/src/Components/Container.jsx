e87967fda596e8ed2ffddfe7534aa7e3e7fa85a1
abhikatkar <katkarabhi91@gmail.com>
2022-05-01 00:25:37 +0530

    worked on css

--- youtube-app/src/Components/Container.jsx
+++ youtube-app/src/Components/Container.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./Container.css";
 import axios from "axios";
-
+import { AiFillGithub } from "react-icons/ai";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { getData, getLoading, getError } from "./Redux/Data/action";
@@ -10,37 +10,55 @@ export const Container = () => {
   const [query, setQuery] = useState("songs");
 
   const dispatch = useDispatch();
-  const { data , loading , error} = useSelector((store) => store.data);
-  
+  const { data, loading, error } = useSelector((store) => store.data);
+  console.log(data);
 
   useEffect(() => {
     getInfo();
-  },[]);
+  }, []);
 
   function getInfo() {
     dispatch(getLoading());
 
     axios
       .get(
-        // "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=viewCount&q=india&type=video&key=AIzaSyBDtD3uMk-u4ZkvmQtJrXrJOIwr5yBvS88"
+        // `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=15&order=viewCount&q=${query}&type=video&key=AIzaSyBDtD3uMk-u4ZkvmQtJrXrJOIwr5yBvS88`
       )
-      .then(({data}) => {
+      .then(({ data }) => {
         dispatch(getData(data.items));
       })
       .catch(() => dispatch(getError()));
   }
 
-  
-
   return (
     <div className="container">
-      <h1>container</h1>
       
-      {data.map()}
+      <div className="allOne">
+      {data.map((e) => {
+        return (
+          
+            <div className="box">
+              <img
+                src={e.snippet.thumbnails.high.url}
+                alt=""
+              />
+              <div className="con">
+                <h1>
+                  <AiFillGithub />
+                </h1>
+                <div className="side">
+                  <h4>{e.snippet.title}</h4>
+                  <p className="channel">{e.snippet.channelTitle}</p>
+                </div>
+              </div>
+            </div>
+          
+        );
+      })}
+      </div>
     </div>
   );
 };
-
 
 // loading ? (
 //   <h1 className="loading">Loading...</h1>
